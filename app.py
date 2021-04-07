2298b3d3500cbed379ecc2af1d92c33d541825d0
aamp19 <51056791+aamp19@users.noreply.github.com>
2021-04-07 18:07:54 -0400

    Update app.py

--- app.py
+++ app.py
@@ -6,18 +6,18 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return render_template('index.html')
+
 class TestCode(unittest.TestCase):
     with open('index.html') as file:
     contents = file.read()
-    search_word = input("enter a word you want to search in file: ")
     storage = []
-    if search_word in contents:
-        storage.append(search_word)
-        storage = "".join(storage)
-    else:
-        print ('word not found')
+    for i in contents:
+        if i == 'Hello World':
+            storage.append(i)
+            storage = "".join(storage)
+       
     def test_greeting(self):
         self.assert_template_used('index.html')
-        self.assert_equal(storage, contents)
+        self.assert_equal(storage, 'Hello World')
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0")
