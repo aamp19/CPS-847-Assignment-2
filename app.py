d35ff141e69056b0bb702ba39701da2ec2f09a86
aamp19 <51056791+aamp19@users.noreply.github.com>
2021-04-07 19:42:49 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,25 @@
 import unittest
 import codecs
+
+
+
 def to_string():
     f = codecs.open("index.html",'r')
     storage = f.read()
-    return (storage)
-               
+    for i in storage.split(" "):
+      if "hello" in i:
+        return True
+  
+
+
+
+
+                
 class TestCode(unittest.TestCase):       
     def test_greeting(self):
-        self.assertEqual(to_string(), 'Hello World!')
+        self.assertEqual(to_string(), True)
+
+
 
 if __name__ == '__main__':
     unittest.main()
