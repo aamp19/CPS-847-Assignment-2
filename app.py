f02488b8c80bc28019feb07ad782ac8c3ebe6305
aamp19 <51056791+aamp19@users.noreply.github.com>
2021-04-08 21:44:08 -0400

    Update app.py

--- app.py
+++ app.py
@@ -11,15 +11,27 @@ def to_string():
         return True
   
 
+def addition():
+    x = 1
+    y = 1
+    c = x+y
+    return c
 
-
-
+def subtraction():
+    x = 2
+    y = 1
+    c = x-y
+    return c
                 
 class TestCode(unittest.TestCase):       
     def test_greeting(self):
         self.assertEqual(to_string(), True)
 
-
+    def test_addition(unittest.TestCase):
+        self.assertEqual(addition(),2)
+    
+    def test_subtraction(unittest.TestCase):
+        self.assertEqual(subtraction(),0)
 
 if __name__ == '__main__':
     unittest.main()
